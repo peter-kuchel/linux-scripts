bfe36380349030694991868dafff0daae1740578
Peter Kuchel <petermichaelkuchel@gmial.com>
2024-11-06 12:34:52 -0700

    adding regex to remove the comma at the end of the last kv pair in swallows

--- save_i3_session.sh
+++ save_i3_session.sh
@@ -7,7 +7,11 @@ fi
 SAVE_FILE=~/.config/i3/ws_$2.json
 echo "save file is $SAVE_FILE"
 i3-save-tree --workspace $1 > $SAVE_FILE
-sed -i 's|^\(\s*\)// "|\1"|g; /^\s*\/\//d ; s/"machine[^\n]*//' $SAVE_FILE
-sed -i 's/"title":[^\n]*// ; s/"window_role":[^\n]*//' $SAVE_FILE
+#tr -d '\n' < $SAVE_FILE > $SAVE_FILE
+sed -i 's|^\(\s*\)// "|\1"|g; /^\s*\/\//d' $SAVE_FILE
+sed -i 's/[\s]*\"machine[^\n]*//g ; s/"title":[^\n]*//g' $SAVE_FILE
+sed -i 's/\(\"instance[^,]*\),/\1/' $SAVE_FILE
+#sed -i 's/
+#sed -i 's/"machine[^\n]*// ; s/"title":[^\n]*// ; s/"window_role":[^\n]*//' $SAVE_FILE
 #sed -i '"title:"^[\n]*' $SAVE_FILE
 #sed -i '"window_role:"^[\n]*' $SAVE_FILE
